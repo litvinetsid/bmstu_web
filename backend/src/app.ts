8502d6f9cca934b1ac42fff2b11f27dad11910b8
axywe <axywewastaken@gmail.com>
2025-01-12 11:45:21 +0300

    ssl

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,3 @@
-import https from 'https';
-import http from 'http';
-import fs from 'fs';
 import dotenv from 'dotenv';
 import express from 'express';
 import { errorMiddleware } from './middlewares/error.middleware';
@@ -13,6 +10,7 @@ const app = express();
 const PORT = Number(process.env.NODE_PORT) || 8080;
 
 app.use(express.json());
+
 app.use(cors({
   origin: `${process.env.CORS_ORIGIN}`,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -20,24 +18,11 @@ app.use(cors({
 }));
 
 app.use('/api/v1', routes);
+
 app.use(errorMiddleware);
 
-const SSL_KEY = process.env.SSL_KEY_PATH;
-const SSL_CERT = process.env.SSL_CERT_PATH;
-
-if (SSL_KEY && SSL_CERT && fs.existsSync(SSL_KEY) && fs.existsSync(SSL_CERT)) {
-  const credentials = {
-    key: fs.readFileSync(SSL_KEY),
-    cert: fs.readFileSync(SSL_CERT),
-  };
-
-  https.createServer(credentials, app).listen(PORT, () => {
-    console.log(`HTTPS Server running on port ${PORT}`);
-  });
-} else {
-  http.createServer(app).listen(PORT, () => {
-    console.log(`HTTP Server running on port ${PORT} ${SSL_CERT} ${SSL_CERT && fs.existsSync(SSL_CERT)}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 export default app;
